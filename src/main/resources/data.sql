988c3350b75e44bd058ec54808b98d1d3b259fd7
Dylan J. Gerrits <gerritsdylan@gmail.com>
2024-12-03 12:53:44 +0100

    Création de data fictives

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO survey (id) VALUES (1);
+
+INSERT INTO question (id, text, survey_id) VALUES
+                                               (1, 'How satisfied are you with the service?', 1),
+                                               (2, 'How likely are you to recommend us?', 1),
+                                               (3, 'How would you rate the quality?', 1),
+                                               (4, 'How was your overall experience?', 1),
+                                               (5, 'How helpful was our staff?', 1);
